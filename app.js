cfa7a1f7d962eadc0abebba6bd1c69f353e856ad
KaungSiThu-Sallius <kaungsithu.sallius@gmail.com>
2023-04-25 13:24:24 +0530

    login ejs

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 /* eslint-disable no-unused-vars */
+
 // initialize express
 const express = require("express");
 const app = express();
+
+//for use json file
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+//for use ejs file
+app.set("view engine", "ejs");
+
+//for css
+app.use(express.static("public"));
+
+//for form post request
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async function (request, response) {
+  response.render("login");
+});
+
+module.exports = app;
