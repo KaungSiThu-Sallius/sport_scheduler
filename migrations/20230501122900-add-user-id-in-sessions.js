4eaffab48e707a00a6ef58e476a5f0fbb4293c2c
KaungSiThu-Sallius <kaungsithu.sallius@gmail.com>
2023-05-03 17:17:35 +0530

    remove userId in migration File3

--- migrations/20230501122900-add-user-id-in-sessions.js
+++ migrations/20230501122900-add-user-id-in-sessions.js
@@ -9,9 +9,17 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn("Sessions", "userId", {
-      type: Sequelize.DataTypes.INTEGER,
-    });
+    // await queryInterface.addColumn("Sessions", "userId", {
+    //   type: Sequelize.DataTypes.INTEGER,
+    // });
+    // await queryInterface.addConstraint("Sessions", {
+    //   fields: ["userId"],
+    //   type: "foreign key",
+    //   references: {
+    //     table: "Users",
+    //     field: "id",
+    //   },
+    // });
   },
 
   async down(queryInterface, Sequelize) {
